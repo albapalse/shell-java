cf3b1d7043cedaa426ac872c7d3bc1c23db191d8
albapalse <albapalse@gmail.com>
2026-06-02 06:11:11 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        System.out.println(command + ": command not found");
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
