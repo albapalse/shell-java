05b2e1ae4615aebaf6922a429d768a375fc6bbf8
albapalse <albapalse@gmail.com>
2026-06-02 06:12:50 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ public class Main {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("$ ");
-
             String input = scanner.nextLine();
             System.out.println(input + ": command not found");
         }
